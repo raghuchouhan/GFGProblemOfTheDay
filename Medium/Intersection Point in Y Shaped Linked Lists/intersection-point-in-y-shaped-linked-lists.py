84c63e3d7a92dd70ea5429d691eb6f7925957049
Raghuveer singh Chouhan <sondhiya79095@gmail.com>
2024-01-18 12:34:55 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Intersection Point in Y Shaped Linked Lists/intersection-point-in-y-shaped-linked-lists.py
@@ -0,0 +1,96 @@
+#User function Template for python3
+def getLength(head):
+    length = 0
+    while head:
+        length += 1
+        head = head.next
+    return length
+
+def intersetPoint(head1, head2):
+    len1 = getLength(head1)
+    len2 = getLength(head2)
+
+    if len1 > len2:
+        for _ in range(len1 - len2):
+            head1 = head1.next
+    elif len2 > len1:
+        for _ in range(len2 - len1):
+            head2 = head2.next
+
+    while head1 and head2:
+        if head1 == head2:
+            return head1.data
+        head1 = head1.next
+        head2 = head2.next
+
+    return -1
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#Contributed by : Nagendra Jha
+
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+# Node Class
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        temp=None
+    
+    # creates a new node with given value and appends it at the end of the linked list
+    def append(self, new_node):
+        if self.head is None:
+            self.head = new_node
+            self.temp = self.head
+            return
+        else:
+            self.temp.next = new_node
+            self.temp = self.temp.next
+
+if __name__ == '__main__':
+    t=int(input())
+    for cases in range(t):
+        x,y,z = map(int,input().strip().split())
+        a = LinkedList()  # create a new linked list 'a'.
+        b = LinkedList()  # create a new linked list 'b'.
+        nodes_a = list(map(int, input().strip().split()))
+        nodes_b = list(map(int, input().strip().split()))
+        nodes_common = list(map(int, input().strip().split()))
+
+        for x in nodes_a:
+            node=Node(x)
+            a.append(node)  # add to the end of the list
+
+        for x in nodes_b:
+            node=Node(x)
+            b.append(node)  # add to the end of the list
+
+        for i in range(len(nodes_common)):
+            node=Node(nodes_common[i])
+            a.append(node)  # add to the end of the list a
+            if i== 0:
+                b.append(node)  # add to the end of the list b, only the intersection
+        
+        print(intersetPoint(a.head,b.head))
+
+# } Driver Code Ends
