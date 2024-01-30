3dcd1aad6acc8bbe292f605101f858977b9e57f8
Raghuveer singh Chouhan <sondhiya79095@gmail.com>
2024-01-30 10:31:25 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Find first set bit/find-first-set-bit.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+public:
+    unsigned int getFirstSetBit(int n)
+    {
+        int cnt = 0;
+        while(n){
+            cnt++;
+            if(n&1)
+                break;
+            n >>= 1;
+        }
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver code
+int main()
+{
+    int t;
+    cin>>t; // testcases
+    while(t--)
+    {
+        int n;
+        cin>>n; //input n
+        Solution ob;
+        printf("%u\n", ob.getFirstSetBit(n)); // function to get answer
+    }
+	return 0;
+}
+
+// } Driver Code Ends
