13f431ac4781f689cc63c7321d468005ee790d50
Raghuveer singh Chouhan <sondhiya79095@gmail.com>
2024-02-21 11:24:26 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Flatten BST to sorted list/flatten-bst-to-sorted-list.java
@@ -0,0 +1,99 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int val) {
+        data = val;
+        left = right = null;
+    }
+}
+
+public class GFG
+{
+    public static void printList(Node head) {
+        while (head != null) {
+            if (head.left != null)
+                System.out.print("-1 ");
+            System.out.print(head.data + " ");
+            head = head.right;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine(); // Consume newline
+        while (t-- > 0) {
+            String str = sc.nextLine();
+            Node root = buildTree(str);
+            Solution ob = new Solution();
+            Node ans = ob.flattenBST(root);
+            printList(ans);
+        }
+        sc.close();
+    }
+
+    private static Node buildTree(String str) {
+        if (str.length() == 0 || str.charAt(0) == 'N')
+            return null;
+
+        String[] ip = str.split("\\s+");
+        int index = 0;
+
+        Node root = new Node(Integer.parseInt(ip[index++]));
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty() && index < ip.length) {
+            Node currNode = queue.poll();
+
+            String currVal = ip[index++];
+            if (!currVal.equals("N")) {
+                currNode.left = new Node(Integer.parseInt(currVal));
+                queue.add(currNode.left);
+            }
+
+            if (index >= ip.length)
+                break;
+            currVal = ip[index++];
+            if (!currVal.equals("N")) {
+                currNode.right = new Node(Integer.parseInt(currVal));
+                queue.add(currNode.right);
+            }
+        }
+
+        return root;
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public Node flattenBST(Node root) {
+        ArrayList<Integer> list = new ArrayList<>();
+        inOrder(root, list);
+        Node dummy = new Node(-1);
+        Node current = dummy;
+        
+        for (int i = 0; i < list.size(); i++) {
+            current.right = new Node(list.get(i));
+            current = current.right;
+        }
+        
+        return dummy.right;
+    }
+    
+    public void inOrder(Node root, ArrayList<Integer> list) {
+        if (root == null)
+            return;
+       
+        inOrder(root.left, list);
+        list.add(root.data);
+        inOrder(root.right, list);
+    }
+}
